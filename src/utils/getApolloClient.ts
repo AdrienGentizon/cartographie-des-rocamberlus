35a7faf707ba840be2b0172fc13950d3b64c7ca5
adrien gentizon <adrien.gentizon@gmail.com>
2021-06-06 11:50:01 +0200

    apollo client with httplink

--- src/utils/getApolloClient.ts
+++ src/utils/getApolloClient.ts
@@ -4,7 +4,12 @@ import {
   ApolloClient,
   NormalizedCacheObject,
   InMemoryCache,
+  createHttpLink,
+  from,
 } from '@apollo/client';
+
+import { onError } from '@apollo/client/link/error';
+import { setContext } from '@apollo/client/link/context';
 
 dotenv.config();
 
@@ -21,10 +26,49 @@ function getEnvVar(
 }
 
 export default function getApolloClient(): ApolloClient<NormalizedCacheObject> {
+  // if (!client) {
+  //   client = new ApolloClient({
+  //     uri: getEnvVar('REACT_APP_APOLLO_SERVER_URI'),
+  //     cache: new InMemoryCache(),
+  //   });
+  // }
+  // return client;
   if (!client) {
-    client = new ApolloClient({
+    const httpLink = createHttpLink({
       uri: getEnvVar('REACT_APP_APOLLO_SERVER_URI'),
-      cache: new InMemoryCache(),
+      credentials: 'include',
+      fetchOptions: {
+        mode: 'cors',
+      },
+    });
+
+    const errorLink = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    });
+
+    const authLink = setContext((_, { headers }) => {
+      // const token = localStorage.getItem(getUserCredentialsLocalStorageKey());
+
+      return {
+        headers: {
+          ...headers,
+          'Access-Control-Allow-Origin': '*',
+          // authorization: token ? `Bearer ${token}` : '',
+        },
+      };
+    });
+
+    client = new ApolloClient({
+      // link: authLink.concat(httpLink),
+      link: from([errorLink, authLink.concat(httpLink)]),
+      cache: new InMemoryCache({}),
     });
   }
   return client;
