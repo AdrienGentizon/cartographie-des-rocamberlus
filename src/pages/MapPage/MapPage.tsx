ff46d343d0aa2ab7317241b93f4d73a73778da29
Adrien Gentizon <adrien.gentizon@gmail.com>
2021-11-01 17:47:16 +0100

    styling

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -3,14 +3,27 @@ import React from "react";
 import Map from "../../components/Map/Map";
 
 import useLocations from "../../contentful/useLocations";
+import { P } from "../../ui";
 
 import Main from "../../ui/Main";
 
 export default function MapPage() {
   const { loading, error, data } = useLocations();
 
-  if (loading) return <p>loading...</p>;
-  if (error) return <p>error !</p>;
+  if (loading)
+    return (
+      <Main>
+        <P>Loading...</P>;
+      </Main>
+    );
+
+  if (error)
+    return (
+      <Main>
+        <P>Error!</P>;
+      </Main>
+    );
+
   if (data)
     return (
       <Main fullWidth>
