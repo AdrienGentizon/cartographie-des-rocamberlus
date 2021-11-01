2e1845c93f31bed27b30d83af7698908ee65bd09
Adrien Gentizon <adrien.gentizon@gmail.com>
2021-11-01 17:19:01 +0100

    fix linting

--- src/components/Nav/MenuButton/MenuButton..tsx
+++ src/components/Nav/MenuButton/MenuButton..tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import { Menu } from '@headlessui/react';
+import { Menu } from "@headlessui/react";
 
-import { ReactComponent as MenuIcon } from './assets/svg/menu-icon.svg';
+import { ReactComponent as MenuIcon } from "./assets/svg/menu-icon.svg";
 
 export function MenuButton() {
   return (
-    <Menu.Button className={`text-gray-600 flex inline-flex justify-end`}>
+    <Menu.Button className={`text-gray-600 flex  justify-end`}>
       <MenuIcon className="text-gray-600" />
     </Menu.Button>
   );
