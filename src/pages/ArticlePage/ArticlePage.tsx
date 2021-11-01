8cfa470a7139c055760802d28308a46a781acffa
Adrien Gentizon <adrien.gentizon@gmail.com>
2021-11-01 17:46:50 +0100

    article page with text

--- src/pages/ArticlePage/ArticlePage.tsx
+++ src/pages/ArticlePage/ArticlePage.tsx
@@ -3,17 +3,41 @@ import { useParams } from "react-router-dom";
 import useArticleFromId from "../../contentful/useArticleFromId";
 import { H1, P } from "../../ui";
 import Main from "../../ui/Main";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
 
 export default function ArticlePage() {
   const { id } = useParams<{ id?: string }>();
 
   const { loading, error, data } = useArticleFromId(id);
-  if (loading) return <P>Loading...</P>;
-  if (error) return <P>Error!</P>;
+
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node: any, children: any) => (
+        <P align="justify">{children}</P>
+      ),
+    },
+  };
+
+  if (loading)
+    return (
+      <Main>
+        <P>Loading...</P>;
+      </Main>
+    );
+
+  if (error)
+    return (
+      <Main>
+        <P>Error!</P>;
+      </Main>
+    );
+
   if (data)
     return (
       <Main>
         <H1>{data.article.title}</H1>
+        {documentToReactComponents(data.article.articleText.json, options)}
       </Main>
     );
   return <></>;
