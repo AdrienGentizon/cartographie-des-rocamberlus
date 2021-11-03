648f4995ba19525463373d3b0cc2af4ca6ad95e9
Adrien Gentizon <adrien.gentizon@gmail.com>
2021-11-03 20:15:20 +0100

    add Asset comp

---
+++ src/ui/Asset.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import useImageFromId from "../contentful/useAssetFromId";
+
+interface PropsType {
+  id: string;
+}
+
+export function Asset({ id }: PropsType) {
+  const { loading, error, data } = useImageFromId(id);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  if (data) return <img src={data.asset.url} alt="" />;
+  return <></>;
+}
