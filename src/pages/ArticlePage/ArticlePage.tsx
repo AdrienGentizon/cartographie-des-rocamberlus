78ccfd7964e6605129fbb4d38c965fc9cf849723
Adrien Gentizon <adrien.gentizon@gmail.com>
2021-12-04 18:57:23 +0100

    update text justify to Article

--- src/pages/ArticlePage/ArticlePage.tsx
+++ src/pages/ArticlePage/ArticlePage.tsx
@@ -1,48 +1,50 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import useArticleFromId from "../../contentful/useArticleFromId";
-import { H1, P, Main, Asset } from "../../ui";
+import React from "react"
+import { useParams } from "react-router-dom"
+import useArticleFromId from "../../contentful/useArticleFromId"
+import { H1, P, Main, Asset } from "../../ui"
 
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS } from "@contentful/rich-text-types"
 
 export default function ArticlePage() {
-  const { id } = useParams<{ id?: string }>();
+  const { id } = useParams<{ id?: string }>()
 
-  const { loading, error, data } = useArticleFromId(id);
+  const { loading, error, data } = useArticleFromId(id)
 
   const options = {
     renderNode: {
       [BLOCKS.PARAGRAPH]: (node: any, children: any) => (
-        <P align="justify">{children}</P>
+        <P className="text-justify">{children}</P>
       ),
       [BLOCKS.EMBEDDED_ASSET]: (node: any, children: any) => {
-        const id = node.data.target.sys.id;
-        return <Asset id={id} />;
+        const id = node.data.target.sys.id
+        return <Asset id={id} />
       },
     },
-  };
+  }
 
   if (loading)
     return (
       <Main>
         <P>Loading...</P>;
       </Main>
-    );
+    )
 
   if (error)
     return (
       <Main>
         <P>Error!</P>;
       </Main>
-    );
+    )
 
   if (data)
     return (
       <Main>
-        <H1>{data.article.title}</H1>
-        {documentToReactComponents(data.article.articleText.json, options)}
+        <article className="flex flex-col gap-8">
+          <H1>{data.article.title}</H1>
+          {documentToReactComponents(data.article.articleText.json, options)}
+        </article>
       </Main>
-    );
-  return <></>;
+    )
+  return <></>
 }
