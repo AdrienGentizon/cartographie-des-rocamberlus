3e16862d2e171a2c3951f69b460b59929f1b2243
Adrien Gentizon <adrien.gentizon@gmail.com>
2021-12-04 19:00:52 +0100

    update App with new components and 404

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,39 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
-import { ApolloProvider } from "@apollo/client";
-import getApolloClient from "../utils/getApolloClient";
+import { ApolloProvider } from "@apollo/client"
+import getApolloClient from "../utils/getApolloClient"
 
-import { CONTACT_URL, HOME_URL, MAP_URL } from "../routes";
+import { CONTACT_URL, HOME_URL, MAP_URL } from "../routes"
 
-import Nav from "./Nav/Nav";
-import HomePage from "../pages/HomePage/HomePage";
-import ContactPage from "../pages/ContactPage/ContactPage";
-import Footer from "./Footer/Footer";
-import MapPage from "../pages/MapPage/MapPage";
-import ArticlePage from "../pages/ArticlePage/ArticlePage";
+import Nav from "./Nav/Nav"
+import HomePage from "../pages/HomePage/HomePage"
+import ContactPage from "../pages/ContactPage/ContactPage"
+import Footer from "./Footer/Footer"
+import MapPage from "../pages/MapPage/MapPage"
+import ArticlePage from "../pages/ArticlePage/ArticlePage"
+import Page404 from "../pages/ErrorPages/Page404"
+import Body from "./Body/Body"
+import Container from "./Container/Container"
 
 export default function App() {
   return (
     <BrowserRouter>
       <ApolloProvider client={getApolloClient()}>
-        <div className="parent min-h-screen flex flex-col lg:max-w-2xl mx-auto">
-          <Nav />
-          <Switch>
-            <Route exact path={HOME_URL} component={HomePage} />
-            <Route path={MAP_URL} component={MapPage} />
-            <Route path={CONTACT_URL} component={ContactPage} />
-            <Route path="/article/:id" component={ArticlePage} />
-          </Switch>
-          <Footer />
-        </div>
+        <Body>
+          <Container>
+            <Nav />
+            <Switch>
+              <Route exact path={HOME_URL} component={HomePage} />
+              <Route path={MAP_URL} component={MapPage} />
+              <Route path={CONTACT_URL} component={ContactPage} />
+              <Route path="/article/:id" component={ArticlePage} />
+              <Route component={Page404} />
+            </Switch>
+            <Footer />
+          </Container>
+        </Body>
       </ApolloProvider>
     </BrowserRouter>
-  );
+  )
 }
