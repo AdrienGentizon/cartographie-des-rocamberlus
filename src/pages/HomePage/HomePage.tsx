77f35ce71261a195c36a4244e54d36bf2f8340fd
Adrien Gentizon <adrien.gentizon@gmail.com>
2021-12-04 18:59:55 +0100

    update text justify to Home

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,35 +1,35 @@
-import React from "react";
+import React from "react"
 
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS } from "@contentful/rich-text-types"
 
-import useHomePage from "../../contentful/useHomePage";
-import { H1, H2, Main, P } from "../../ui";
+import useHomePage from "../../contentful/useHomePage"
+import { H1, H2, Main, P } from "../../ui"
 
 export default function HomePage() {
-  const { loading, error, data } = useHomePage();
+  const { loading, error, data } = useHomePage()
 
   const options = {
     renderNode: {
       [BLOCKS.PARAGRAPH]: (node: any, children: any) => (
-        <P align="justify">{children}</P>
+        <P className="text-justify">{children}</P>
       ),
     },
-  };
+  }
 
   if (loading)
     return (
       <Main>
         <P>Loading...</P>;
       </Main>
-    );
+    )
 
   if (error)
     return (
       <Main>
         <P>Error!</P>;
       </Main>
-    );
+    )
   if (data)
     return (
       <Main>
@@ -37,6 +37,6 @@ export default function HomePage() {
         <H2>{data.homePage.mainTextTitle}</H2>
         {documentToReactComponents(data.homePage.mainText.json, options)}
       </Main>
-    );
-  return <></>;
+    )
+  return <></>
 }
