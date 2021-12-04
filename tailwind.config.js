0fea892e35c595436b755d81dbed94daee049b8e
Adrien Gentizon <adrien.gentizon@gmail.com>
2021-12-04 19:01:23 +0100

    add noised bg into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        noise:
+          "linear-gradient(135deg, #008ba2b5, #00ac94cf),  url('./assets/images/bg-noise.svg')",
+        noise2: "url('./assets/images/bg-noise.svg')",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
