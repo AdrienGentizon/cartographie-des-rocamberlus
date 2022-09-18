c4d46302f5b66472d660ba1f7dcc8361ed560840
Adrien Gentizon <adrien.gentizon@gmail.com>
2022-09-18 14:31:01 +0200

    fix map title lisibility

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,19 @@
 import React from "react"
+import { useLocation } from "react-router-dom"
 import Nav from "../Nav/Nav"
 import Title from "./Title/Title"
 
 export default function Header() {
+  const { pathname } = useLocation()
+  const darkMode = pathname === "/map"
+  console.log(darkMode)
+
   return (
     <header
       className={`
       flex flex-col justify-center
       z-10
+      ${darkMode ? "text-white mix-blend-difference" : ""}
       `}
     >
       <Title />
