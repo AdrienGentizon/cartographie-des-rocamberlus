d8bdff7c21ff64d23e3243d0e5114dc7532fd213
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-22 16:43:18 +0100

    hide footer on mobile

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="z-50 bottom-0 text-xs text-center text-gray-500 font-thin py-6">
-      Une présentation de{" "}
+    <footer className="hidden lg:flex z-50 bottom-0 text-xs text-center text-gray-500 font-thin py-6">
+      Une présentation de{' '}
       <em className="text-gray-700 not-italic">Le Bureau des Tentatives</em>,
       2021.
     </footer>
