296c611d5439c22118beb621c0954185b2b63ea5
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-22 16:42:58 +0100

    remove app bg

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,9 +1,9 @@
-import React from "react"
+import React from 'react'
 
 interface PropsType {
   children: React.ReactNode
 }
 
 export default function Body({ children }: PropsType) {
-  return <div className="bg-noise bg-blend-color-burn">{children}</div>
+  return <div className="bg-white">{children}</div>
 }
