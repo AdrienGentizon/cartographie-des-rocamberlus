52b7e0510373ec735b53348b449f8ee12f6bf1ae
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-22 16:42:32 +0100

    refacto nav bar

--- src/components/Nav/MenuItem/MenuItem.tsx
+++ src/components/Nav/MenuItem/MenuItem.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React from 'react'
 
-import { NavLink } from "react-router-dom"
+import { NavLink } from 'react-router-dom'
 
 interface MenuItemProps {
   url: string
@@ -9,18 +9,11 @@ interface MenuItemProps {
 
 export default function MenuItem({ url, title }: MenuItemProps) {
   return (
-    <li className={`flex`}>
+    <li className="flex">
       <NavLink
         exact
-        activeClassName={`border-gray-400 border-b`}
-        className={`
-        pt-2 pb-1 px-4
-        border-b-2 border-transparent
-          
-      hover:border-gray-600 hover:text-gray-600
-      
-        transition-all ease-in-out duration-500
-      `}
+        activeClassName="border-gray-400 border-b"
+        className="pt-2 pb-1 px-4 border-b-2 border-transparent hover:border-gray-600 hover:text-gray-600 transition-all ease-in-out duration-500"
         to={url}
       >
         {title}
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,12 @@
-import React from "react"
+import React from 'react'
 
-import { CONTACT_URL, HOME_URL, MAP_URL } from "../../routes"
-import MenuItem from "./MenuItem/MenuItem"
+import { CONTACT_URL, HOME_URL, MAP_URL } from '../../routes'
+import MenuItem from './MenuItem/MenuItem'
 
 export default function Nav() {
   return (
     <nav>
-      <ul
-        className={`
-      flex justify-center gap-8
-      w-full h-full
-      `}
-      >
+      <ul className="flex justify-center gap-8 w-full h-full">
         <MenuItem url={HOME_URL} title="Home" />
         <MenuItem url={MAP_URL} title="Carte" />
         <MenuItem url={CONTACT_URL} title="Contact" />
