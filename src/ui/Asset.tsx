2902d8b9a64ac614eb811d94c3d8e67e634c135f
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-23 10:58:46 +0100

    add loading state to img

--- src/ui/Asset.tsx
+++ src/ui/Asset.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useImageFromId from '../contentful/useAssetFromId'
 
 export function Img({
   src,
   alt = '',
   className = '',
+  loading = false,
 }: {
   src: string
   alt?: string
   className?: string
+  loading?: boolean
 }) {
+  const [imageLoading, setImageLoading] = useState(true)
+  const isLoading = loading || imageLoading
+
   return (
-    <img
-      src={src}
-      alt={alt}
-      className={`border-2 border-gray-100 bg-gray-50 p-1 rounded ${className}`}
-    />
+    <>
+      {isLoading && <span className="flex mx-auto my-2 loader" />}
+      <img
+        src={src}
+        alt={alt}
+        className={`${
+          isLoading ? 'invisible' : ''
+        } border-2 border-gray-100 bg-gray-50 p-1 rounded ${className}`}
+        onLoad={() => setImageLoading(false)}
+      />
+    </>
   )
 }
 
@@ -27,8 +38,10 @@ interface PropsType {
 export function Asset({ id, alt }: PropsType) {
   const { loading, error, data } = useImageFromId(id)
 
-  if (loading) return <p className="font-extralight">Loading...</p>
-  if (error) return <p>Error!</p>
+  if (error) console.error(error.message)
+  if (error) return <></>
   if (!data) return <></>
-  return <Img src={data.asset.url} alt={alt} className="my-3" />
+  return (
+    <Img src={data.asset.url} alt={alt} className="my-3" loading={loading} />
+  )
 }
