de3558d110355980753ce8f62381a56e6da380bb
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-23 10:31:09 +0100

    center footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="hidden lg:flex z-50 bottom-0 text-xs text-center text-gray-500 font-thin py-6">
-      Une présentation de{' '}
-      <em className="text-gray-700 not-italic">Le Bureau des Tentatives</em>,
-      2021.
+    <footer className="hidden lg:flex justify-center z-50 bottom-0 text-xs text-center text-gray-500 font-thin py-6">
+      Une présentation de&nbsp;
+      <em className="text-gray-700 not-italic">Le Bureau des Tentatives</em>
+      ,&nbsp;2021.
     </footer>
   )
 }
