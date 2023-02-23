a9d349fea1d5c9dfe67cefaf65143df414e7c162
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-23 15:49:39 +0100

    update asset getter with optional resize

--- src/contentful/useAssetFromId.ts
+++ src/contentful/useAssetFromId.ts
@@ -1,15 +1,45 @@
-import { gql, useQuery } from '@apollo/client'
-import { PictureFragment } from './ContentfulFragments'
+import { ApolloError, gql, useQuery } from '@apollo/client'
 
-const GET_IMAGE_FORM_ID_QUERY = gql`
-  ${PictureFragment}
+const getImageFromIdQuery = (options?: { width?: number }) => gql`
   query getAssetFromId($id: String!) {
     asset(id: $id) {
-      ...PictureFragment
+      sys {
+        id
+      }
+      title
+      description
+      contentType
+      fileName
+      ${options?.width ? `url(transform: { width: ${options.width} })` : 'url'}
+      size
+      width
+      height
     }
   }
 `
 
-export default function useImageFromId(id: string) {
-  return useQuery(GET_IMAGE_FORM_ID_QUERY, { variables: { id } })
+interface AssetType {
+  contentType: string
+  description: string
+  fileName: string
+  height: number
+  width: number
+  size: number
+  title: string
+  url: string
+}
+
+export default function useImageFromId(id: string): {
+  image?: AssetType
+  loading: boolean
+  error?: ApolloError
+} {
+  const { data, error, loading } = useQuery<{ asset?: AssetType }>(
+    getImageFromIdQuery({ width: 720 }),
+    {
+      variables: { id },
+    }
+  )
+
+  return { image: data?.asset, error, loading }
 }
