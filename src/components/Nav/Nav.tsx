a8966f905b8e9e980e535254932f4b27c2bda165
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-23 12:34:40 +0100

    add search to navbar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import useArtists from '../../contentful/useArtists'
 
 import { CONTACT_URL, HOME_URL, MAP_URL } from '../../routes'
 import MenuItem from './MenuItem/MenuItem'
+import Search from './Search/Search'
 
 export default function Nav() {
+  const { artists } = useArtists()
+
   return (
     <nav>
       <ul className="flex justify-center gap-8 w-full h-full">
         <MenuItem url={HOME_URL} title="Home" />
         <MenuItem url={MAP_URL} title="Carte" />
         <MenuItem url={CONTACT_URL} title="Contact" />
+        <Search artists={artists} />
       </ul>
     </nav>
   )
