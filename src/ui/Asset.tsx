c5798ee81ddbf17fc472114e59adbdcaa84eba6e
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-23 10:31:24 +0100

    update asset with sharable root Img comp

--- src/ui/Asset.tsx
+++ src/ui/Asset.tsx
@@ -1,22 +1,34 @@
-import React from "react"
-import useImageFromId from "../contentful/useAssetFromId"
+import React from 'react'
+import useImageFromId from '../contentful/useAssetFromId'
+
+export function Img({
+  src,
+  alt = '',
+  className = '',
+}: {
+  src: string
+  alt?: string
+  className?: string
+}) {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={`border-2 border-gray-100 bg-gray-50 p-1 rounded ${className}`}
+    />
+  )
+}
 
 interface PropsType {
   id: string
+  alt?: string
 }
 
-export function Asset({ id }: PropsType) {
+export function Asset({ id, alt }: PropsType) {
   const { loading, error, data } = useImageFromId(id)
 
-  if (loading) return <p>Loading...</p>
+  if (loading) return <p className="font-extralight">Loading...</p>
   if (error) return <p>Error!</p>
-  if (data)
-    return (
-      <img
-        src={data.asset.url}
-        alt=""
-        className="my-4 border-2 border-gray-100 bg-gray-50 p-1 rounded"
-      />
-    )
-  return <></>
+  if (!data) return <></>
+  return <Img src={data.asset.url} alt={alt} className="my-3" />
 }
