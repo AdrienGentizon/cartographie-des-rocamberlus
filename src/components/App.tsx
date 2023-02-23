3c69dd9208d18250bcef2b30e245727fa0035a95
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-23 20:41:34 +0100

    add random particles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,37 +1,37 @@
-import React from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import { ApolloProvider } from "@apollo/client"
-import getApolloClient from "../utils/getApolloClient"
+import { ApolloProvider } from '@apollo/client'
+import getApolloClient from '../utils/getApolloClient'
 
-import { CONTACT_URL, HOME_URL, MAP_URL } from "../routes"
+import { CONTACT_URL, HOME_URL, MAP_URL } from '../routes'
 
-import HomePage from "../pages/HomePage/HomePage"
-import ContactPage from "../pages/ContactPage/ContactPage"
-import Footer from "./Footer/Footer"
-import MapPage from "../pages/MapPage/MapPage"
-import ArticlePage from "../pages/ArticlePage/ArticlePage"
-import Page404 from "../pages/ErrorPages/Page404"
-import Body from "./Body/Body"
-import Container from "./Container/Container"
-import Header from "./Header/Header"
+import HomePage from '../pages/HomePage/HomePage'
+import ContactPage from '../pages/ContactPage/ContactPage'
+import Footer from './Footer/Footer'
+import MapPage from '../pages/MapPage/MapPage'
+import ArticlePage from '../pages/ArticlePage/ArticlePage'
+import Page404 from '../pages/ErrorPages/Page404'
+import Body from './Body/Body'
+import Container from './Container/Container'
+import Header from './Header/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
       <ApolloProvider client={getApolloClient()}>
         <Body>
-          <Container>
-            <Header />
-            <Switch>
+          <Switch>
+            <Container>
+              <Header />
               <Route exact path={HOME_URL} component={HomePage} />
               <Route path={MAP_URL} component={MapPage} />
               <Route path={CONTACT_URL} component={ContactPage} />
               <Route path="/article/:id" component={ArticlePage} />
               <Route component={Page404} />
-            </Switch>
-            <Footer />
-          </Container>
+              <Footer />
+            </Container>
+          </Switch>
         </Body>
       </ApolloProvider>
     </BrowserRouter>
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,13 +1,71 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import useImageFromId from '../../contentful/useAssetFromId'
 
 interface PropsType {
   children: React.ReactNode
 }
-
+const FLOWER_ID = '4AS9TKfECMPrNfmBPpplES'
+const GRASS_ID = '5nX6KH1GrITrptF6pIMUz5'
+const PARTICLE_DENSITY = 16
 export default function Container({ children }: PropsType) {
+  const history = useHistory()
+  const { image: flower } = useImageFromId(FLOWER_ID)
+  const { image: grass } = useImageFromId(GRASS_ID)
+  const [particles, setParticles] = useState<{ top: number; left: number }[]>(
+    []
+  )
+  const [containerRef, setContainerRef] = useState<HTMLDivElement | null>(null)
+  const [containerHeight, setContainerHeight] = useState(0)
+  const resizeObs = useRef(
+    new ResizeObserver((entries) => {
+      if (entries.length > 0) {
+        setContainerHeight(entries[0].target.getBoundingClientRect().height)
+      }
+    })
+  )
+  useEffect(() => {
+    if (containerRef) resizeObs.current.observe(containerRef)
+  }, [containerRef, resizeObs])
+  useEffect(() => {
+    const particles: { top: number; left: number }[] = []
+    if (containerHeight > 0) {
+      const ratio = Math.floor(containerHeight / window.innerHeight)
+      for (let n = 0; n < PARTICLE_DENSITY * ratio; n++) {
+        particles.push({
+          top: ratio * window.innerHeight * Math.random(),
+          left: window.innerWidth * Math.random(),
+        })
+      }
+      setParticles(particles)
+    }
+  }, [containerHeight, flower, grass, history.location.pathname])
   return (
-    <div className="bg-white min-h-screen flex flex-col lg:max-w-2xl mx-auto">
-      {children}
-    </div>
+    <>
+      <div className="border w-screen absolute">
+        {flower &&
+          grass &&
+          particles.map(({ top, left }, n: number) => {
+            return (
+              <img
+                key={`flower-${n}`}
+                className="absolute w-8"
+                style={{
+                  top,
+                  left,
+                }}
+                alt="flower"
+                src={Math.random() > 0.75 ? flower.url : grass.url}
+              />
+            )
+          })}
+      </div>
+      <div
+        ref={setContainerRef}
+        className="relative bg-white min-h-screen h-full flex flex-col lg:max-w-2xl mx-auto border"
+      >
+        {children}
+      </div>
+    </>
   )
 }
