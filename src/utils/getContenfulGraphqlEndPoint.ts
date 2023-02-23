91237117302cc4dc7d89d0c815421d9ce7e6540a
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-23 17:49:48 +0100

    remove unused file

--- src/utils/getContenfulGraphqlEndPoint.ts
+++
@@ -1,7 +0,0 @@
-import getEnv from "./getEnv";
-
-export default function getContentfulGraphqlEndPoint(): string {
-  return `${getEnv().REACT_APP_CONTENTFUL_GRAPHQL_ENDPOINT}/${
-    getEnv().REACT_APP_CONTENTFUL_SPACE_ID
-  }/explore?access_token=${getEnv().REACT_APP_CONTENTFUL_API_KEY}`;
-}
