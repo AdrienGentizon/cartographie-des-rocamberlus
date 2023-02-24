1094ace7343e98ecbaeb5f7cb92b042322194d1d
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-24 10:47:29 +0100

    update container component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,20 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import useImageFromId from '../../contentful/useAssetFromId'
+import BackGroundRandom from './BackGroundRandom/BackGroundRandom'
 
 interface PropsType {
   children: React.ReactNode
 }
-const FLOWER_ID = '4AS9TKfECMPrNfmBPpplES'
-const GRASS_ID = '5nX6KH1GrITrptF6pIMUz5'
-const PARTICLE_DENSITY = 16
+
 export default function Container({ children }: PropsType) {
   const history = useHistory()
-  const { image: flower } = useImageFromId(FLOWER_ID)
-  const { image: grass } = useImageFromId(GRASS_ID)
-  const [particles, setParticles] = useState<{ top: number; left: number }[]>(
-    []
-  )
   const [containerRef, setContainerRef] = useState<HTMLDivElement | null>(null)
   const [containerHeight, setContainerHeight] = useState(0)
   const resizeObs = useRef(
@@ -26,40 +19,11 @@ export default function Container({ children }: PropsType) {
   )
   useEffect(() => {
     if (containerRef) resizeObs.current.observe(containerRef)
-  }, [containerRef, resizeObs])
-  useEffect(() => {
-    const particles: { top: number; left: number }[] = []
-    if (containerHeight > 0) {
-      const ratio = Math.floor(containerHeight / window.innerHeight)
-      for (let n = 0; n < PARTICLE_DENSITY * ratio; n++) {
-        particles.push({
-          top: ratio * window.innerHeight * Math.random(),
-          left: window.innerWidth * Math.random(),
-        })
-      }
-      setParticles(particles)
-    }
-  }, [containerHeight, flower, grass, history.location.pathname])
+  }, [containerRef, resizeObs, history.location.pathname])
+
   return (
     <>
-      <div className="w-screen absolute">
-        {flower &&
-          grass &&
-          particles.map(({ top, left }, n: number) => {
-            return (
-              <img
-                key={`flower-${n}`}
-                className="hidden lg:block absolute w-8"
-                style={{
-                  top,
-                  left,
-                }}
-                alt="flower"
-                src={Math.random() > 0.75 ? flower.url : grass.url}
-              />
-            )
-          })}
-      </div>
+      <BackGroundRandom containerHeight={containerHeight} />
       <div
         ref={setContainerRef}
         className="relative bg-white min-h-screen h-full flex flex-col lg:max-w-3xl mx-auto shadow"
