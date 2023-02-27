d81e3a9b6aee43f4cf38ac76598844f8cea7203e
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-02-27 17:07:56 +0100

    update nav bar layout

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import useArtists from '../../contentful/useArtists'
 
 import { CONTACT_URL, HOME_URL, MAP_URL } from '../../routes'
@@ -6,16 +7,21 @@ import MenuItem from './MenuItem/MenuItem'
 import Search from './Search/Search'
 
 export default function Nav() {
+  const history = useHistory()
   const { artists } = useArtists()
 
   return (
-    <nav>
+    <nav className="flex flex-col">
       <ul className="flex justify-center gap-8 w-full h-full">
-        <MenuItem url={HOME_URL} title="Home" />
+        <MenuItem url={HOME_URL} title="Maison" />
         <MenuItem url={MAP_URL} title="Carte" />
         <MenuItem url={CONTACT_URL} title="Contact" />
-        <Search artists={artists} />
       </ul>
+      {!history.location.pathname.startsWith('/map') && (
+        <div className="px-32">
+          <Search artists={artists} />
+        </div>
+      )}
     </nav>
   )
 }
