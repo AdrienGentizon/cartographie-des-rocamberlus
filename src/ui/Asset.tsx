c3f2958da2987e673a54facce5f5c51fd58fe700
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-03-09 19:17:58 +0100

    update asset picture frame

--- src/ui/Asset.tsx
+++ src/ui/Asset.tsx
@@ -15,26 +15,50 @@ export function Img({
   const [imageLoading, setImageLoading] = useState(true)
   const isLoading = loading || imageLoading
 
-  return (
-    <>
-      {isLoading && (
-        <div
-          className={`border-2 border-gray-100 bg-gray-50 p-1 rounded ${className}`}
-        >
-          <div className="flex justify-center bg-white  mx-auto  h-20 max-h-20 p-4 w-20 min-w-full">
-            <span className=" block loader" />
-          </div>
+  if (isLoading)
+    return (
+      <div
+        className={`border-2 border-gray-100 bg-gray-50 p-1 rounded ${className}`}
+      >
+        <div className="flex justify-center bg-white  mx-auto  h-20 max-h-20 p-4 w-20 min-w-full">
+          <span className=" block loader" />
         </div>
-      )}
+        <img
+          src={src}
+          alt={alt}
+          className={`invisible h-0`}
+          onLoad={() => setImageLoading(false)}
+        />
+      </div>
+    )
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: 'fit-content',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 8,
+        borderImageOutset: 0,
+        borderImageSource: 'url(/picture-frame.png)',
+        borderImageSlice: 16,
+        borderImageRepeat: 'round',
+        borderImageWidth: 1.5,
+      }}
+      className={className}
+    >
       <img
         src={src}
         alt={alt}
-        className={`${
-          isLoading ? 'invisible h-0' : ''
-        } border-2 border-gray-100 bg-gray-50 p-1 rounded ${className}`}
+        style={{
+          boxShadow: 'inset 0 0 10px white',
+          borderRadius: 6,
+          padding: 2,
+        }}
         onLoad={() => setImageLoading(false)}
       />
-    </>
+    </div>
   )
 }
 
@@ -52,7 +76,7 @@ export function Asset({ id, alt }: PropsType) {
     <Img
       src={image.url}
       alt={alt}
-      className="my-3 mx-auto min-w-full"
+      className="my-3 mx-auto "
       loading={loading}
     />
   )
