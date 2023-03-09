d22bc7027ff6ccc8d4e902d697032ae814233be2
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-03-09 12:44:54 +0100

    update background layout logic

--- src/components/Layout/Container/BackGroundRandom/BackGroundRandom.tsx
+++ src/components/Layout/Container/BackGroundRandom/BackGroundRandom.tsx
@@ -1,14 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import useImageFromId from '../../../../contentful/useAssetFromId'
+import useDebounce from '../../../../hooks/useDebounce'
 
 const MILL_ID = '3agj2s4Uqz2gIjkxAoVIGy'
-const WELL_ID = '7wxSIV2ollwz7lEAxlbpt4'
-const STATUE_ID = '4ImM2j4tL1z5Yg7yuiz7SR'
+const BORIE_ID = '7wxSIV2ollwz7lEAxlbpt4'
+const VENUS_ID = '4ImM2j4tL1z5Yg7yuiz7SR'
 const DEER_ID = '2WlIf3SOvzVVg9NraIuDfd'
 const ELEFANT_ID = '63SBdGZHOxTTFg4bheOY24'
-const SKULL_ID = '1gOCGnkUJB0OJtP3rHBeOQ'
 const TRUELLE_ID = '65APH11IpPHVkyQD1WsuXc'
-const GRID_ROW_HEIGHT = 280
+const BROUETTE_ID = '3TgLWOnNssc61Cc32QO0wK'
+const KING_ID = '5x2dGJ9NUm1pjaMhQnUaXB'
+const FAMILY_ID = '5r5TVJ2Uk2ja0vFRoc0y63'
+
+const CELL_SIZE = 96
+const ITEM_DENSITY = 0.8
+const ITEM_ROTATION = 20
 
 interface PropsType {
   containerHeight: number
@@ -21,15 +27,15 @@ export default function BackGroundRandom({ containerHeight }: PropsType) {
     error: errorMill,
   } = useImageFromId(MILL_ID)
   const {
-    image: well,
-    loading: loadingWell,
-    error: errorWell,
-  } = useImageFromId(WELL_ID)
+    image: borie,
+    loading: loadingBorie,
+    error: errorBorie,
+  } = useImageFromId(BORIE_ID)
   const {
-    image: statue,
-    loading: loadingStatue,
-    error: errorStatue,
-  } = useImageFromId(STATUE_ID)
+    image: venus,
+    loading: loadingVenus,
+    error: errorVenus,
+  } = useImageFromId(VENUS_ID)
   const {
     image: deer,
     loading: loadingDeer,
@@ -41,94 +47,110 @@ export default function BackGroundRandom({ containerHeight }: PropsType) {
     error: errorElefant,
   } = useImageFromId(ELEFANT_ID)
   const {
-    image: skull,
-    loading: loadingSkull,
-    error: errorSkull,
-  } = useImageFromId(SKULL_ID)
+    image: king,
+    loading: loadingKing,
+    error: errorKing,
+  } = useImageFromId(KING_ID)
   const {
     image: truelle,
     loading: loadingTruelle,
     error: errorTruelle,
   } = useImageFromId(TRUELLE_ID)
+  const {
+    image: brouette,
+    loading: loadingBrouette,
+    error: errorBrouette,
+  } = useImageFromId(BROUETTE_ID)
+  const {
+    image: family,
+    loading: loadingFamily,
+    error: errorFamily,
+  } = useImageFromId(FAMILY_ID)
 
-  const [particles, setParticles] = useState<
-    { top: number; left?: number; right?: number }[]
-  >([])
+  const [cells, setCells] = useState(0)
+
   const loading =
     loadingMill ||
-    loadingWell ||
-    loadingStatue ||
+    loadingBorie ||
+    loadingVenus ||
     loadingDeer ||
     loadingElefant ||
-    loadingSkull ||
-    loadingTruelle
+    loadingKing ||
+    loadingTruelle ||
+    loadingBrouette ||
+    loadingFamily
   const error =
     errorMill ||
-    errorWell ||
-    errorStatue ||
+    errorBorie ||
+    errorVenus ||
     errorDeer ||
     errorElefant ||
-    errorSkull ||
-    errorTruelle
-  const getRandomSource = () => {
-    const alea = Math.random()
-    if (alea < 0.4) return truelle?.url ?? ''
-    if (alea < 0.5) return statue?.url ?? ''
-    if (alea < 0.6) return deer?.url ?? ''
-    if (alea < 0.7) return elefant?.url ?? ''
-    if (alea < 0.8) return well?.url ?? ''
-    if (alea < 0.9) return mill?.url ?? ''
-    return skull?.url ?? ''
-  }
+    errorKing ||
+    errorTruelle ||
+    errorBrouette ||
+    errorFamily
 
-  useEffect(() => {
-    const particles: { top: number; left?: number; right?: number }[] = []
-    if (containerHeight > 0) {
-      for (
-        let n = 0;
-        n < containerHeight - (GRID_ROW_HEIGHT - 96);
-        n += GRID_ROW_HEIGHT
-      ) {
-        particles.push({
-          top: 150 * Math.random(),
-          left: 256 * Math.random() - 48,
-          right: undefined,
-        })
-        particles.push({
-          top: 150 * Math.random(),
-          left: undefined,
-          right: 256 * Math.random() - 48,
-        })
-      }
-      setParticles(particles)
-    }
-  }, [containerHeight, well, mill, statue, deer])
+  const images = [
+    venus,
+    truelle,
+    elefant,
+    deer,
+    mill,
+    borie,
+    family,
+    brouette,
+    king,
+  ]
+
+  const nColumns = Math.floor(window.innerWidth / CELL_SIZE)
+  const nRows = Math.floor(containerHeight / CELL_SIZE)
+  const debounce = useDebounce(() => setCells(nColumns * nRows), 1000)
+  debounce()
+
   if (loading || error) return <></>
   return (
-    <div className="w-screen absolute grid grid-cols-2 gap-x-80">
-      {particles.map(({ top, left, right }, n: number) => {
+    <div
+      className="w-screen absolute"
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat( auto-fill, minmax(${CELL_SIZE}px, 1fr) )`,
+        gridTemplateRows: `repeat( auto-fit, minmax(${CELL_SIZE}px, 1fr) )`,
+        placeItems: 'center',
+        minHeight: containerHeight,
+      }}
+    >
+      {[...Array(cells)].map((_, n: number) => {
+        const source = images.at(Math.floor(images.length * Math.random()))
+        const alea = Math.random()
+        if (!source || alea < ITEM_DENSITY)
+          return (
+            <div
+              key={`particle-${n}`}
+              style={{
+                width: CELL_SIZE,
+                height: CELL_SIZE,
+              }}
+            />
+          )
         return (
           <div
             key={`particle-${n}`}
             style={{
-              height: GRID_ROW_HEIGHT,
-              width: '100%',
+              width: CELL_SIZE,
+              height: CELL_SIZE,
             }}
-            className="hidden lg:block relative"
           >
             <img
-              style={{
-                top,
-                left,
-                right,
-                rotate: `${
-                  (Math.random() > 0.5 ? 1 : -1) * 4 * Math.random()
-                }deg`,
-                transform: Math.random() > 0.5 ? `rotateY(180deg)` : undefined,
-              }}
-              className="absolute"
               alt="particle"
-              src={getRandomSource()}
+              style={{
+                maxHeight: CELL_SIZE,
+                marginInline: 'auto',
+                scale: `${Math.max(0.5, Math.random())}`,
+                rotate: `${ITEM_ROTATION * (-1 * Math.random() + 0.5)}deg`,
+                transform: Math.random() > 0.5 ? `rotateY(180deg)` : undefined,
+                animation: 'fade-in ease-in-out 300ms',
+              }}
+              src={source.url}
             />
           </div>
         )
