5ee4ffeae880898e4264ec3faa4c4711b93b69ca
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-03-10 12:22:47 +0100

    use new ui Dialog instead od headless ui

--- src/pages/ContactPage/Contribution/SubmissionError/SubmissionError.tsx
+++ src/pages/ContactPage/Contribution/SubmissionError/SubmissionError.tsx
@@ -1,62 +1,32 @@
-import { Dialog, Transition } from '@headlessui/react';
-import React, { useState } from 'react';
+import { Dialog } from '../../../../ui'
 
 interface SubmissionErrorProps {
-  setSubmissionError: React.Dispatch<React.SetStateAction<boolean>>;
+  setSubmissionError: (status: boolean) => void
 }
 
 export default function SubmissionError({
   setSubmissionError,
 }: SubmissionErrorProps) {
-  const [isOpen, setIsOpen] = useState(true);
-
   const onClose = () => {
-    setIsOpen(false);
-    setSubmissionError(false);
-  };
+    setSubmissionError(false)
+  }
 
   return (
-    <Transition
-      show={isOpen}
-      enter="transition duration-200 ease-out"
-      enterFrom="transform scale-95 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-125 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-95 opacity-0"
-    >
-      <Dialog
-        open={isOpen}
-        onClose={onClose}
-        className="fixed z-10 inset-0 overflow-y-auto"
+    <Dialog onClose={onClose}>
+      <p className="text-xl text-red-500">Echec de l'envoi</p>
+      <p className="italic text-sm text-red-500">Essayer à nouveau</p>
+
+      <p className="text-sm my-4">
+        Une erreur s'est produite sur le serveur. Essayez à nouveau.
+      </p>
+      <h2>ozihnrgfonzfgonzrognzerog</h2>
+
+      <button
+        onClick={onClose}
+        className="border rounded w-full py-1 text-sm uppercase font-thin bg-gray-100 my-8"
       >
-        <div className="flex items-center justify-center min-h-screen">
-          <Dialog.Overlay className="fixed inset-0 bg-black opacity-20" />
-
-          <div
-            className="bg-white shadox-sm rounded z-20 mx-6 px-12 py-12
-          flex flex-col items-center gap-2 font-thin text-center"
-          >
-            <Dialog.Title className="text-xl text-red-500">
-              Echec de l'envoi
-            </Dialog.Title>
-            <Dialog.Description className="italic text-sm text-red-500">
-              Essayer à nouveau
-            </Dialog.Description>
-
-            <p className="text-sm my-4">
-              Une erreur s'est produite sur le serveur. Essayez à nouveau.
-            </p>
-
-            <button
-              onClick={onClose}
-              className="border rounded w-full py-1 text-sm uppercase font-thin bg-gray-100 my-8"
-            >
-              Retour
-            </button>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
-  );
+        Retour
+      </button>
+    </Dialog>
+  )
 }
--- src/pages/ContactPage/Contribution/SubmissionSuccess/SubmissionSuccess.tsx
+++ src/pages/ContactPage/Contribution/SubmissionSuccess/SubmissionSuccess.tsx
@@ -1,64 +1,35 @@
-import { Dialog, Transition } from '@headlessui/react';
-import React, { useState } from 'react';
+import { Dialog } from '../../../../ui'
 
 interface SubmissionSuccessProps {
-  setSubmissionSuccess: React.Dispatch<React.SetStateAction<boolean>>;
+  setSubmissionSuccess: (status: boolean) => void
 }
 
 export default function SubmissionSuccess({
   setSubmissionSuccess,
 }: SubmissionSuccessProps) {
-  const [isOpen, setIsOpen] = useState(true);
-
   const onClose = () => {
-    setIsOpen(false);
-    setSubmissionSuccess(false);
-  };
+    setSubmissionSuccess(false)
+  }
 
   return (
-    <Transition
-      show={isOpen}
-      enter="transition duration-200 ease-out"
-      enterFrom="transform scale-95 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-125 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-95 opacity-0"
-    >
-      <Dialog
-        open={isOpen}
-        onClose={onClose}
-        className="fixed z-10 inset-0 overflow-y-auto"
+    <Dialog onClose={onClose}>
+      <p className="text-xl text-green-500">Succés de l'envoi</p>
+      <p className="italic text-sm text-green-500">
+        Votre contribution a bien été envoyée
+      </p>
+
+      <p className="text-sm my-4">
+        Notre équipe va prendre en considération votre contribution. Nous vous
+        tiendrons au courant si celle-ci est prise en compte sur notre site.
+        Merci de voter soutien.
+      </p>
+
+      <button
+        onClick={onClose}
+        className="border rounded w-full py-1 text-sm uppercase font-thin bg-gray-100 my-8"
       >
-        <div className="flex items-center justify-center min-h-screen">
-          <Dialog.Overlay className="fixed inset-0 bg-black opacity-20" />
-
-          <div
-            className="bg-white shadox-sm rounded z-20 mx-6 px-12 py-12
-          flex flex-col items-center gap-2 font-thin text-center"
-          >
-            <Dialog.Title className="text-xl text-green-500">
-              Succés de l'envoi
-            </Dialog.Title>
-            <Dialog.Description className="italic text-sm text-green-500">
-              Votre contribution a bien été envoyée
-            </Dialog.Description>
-
-            <p className="text-sm my-4">
-              Notre équipe va prendre en considération votre contribution. Nous
-              vous tiendrons au courant si celle-ci est prise en compte sur
-              notre site. Merci de voter soutien.
-            </p>
-
-            <button
-              onClick={onClose}
-              className="border rounded w-full py-1 text-sm uppercase font-thin bg-gray-100 my-8"
-            >
-              Retour
-            </button>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
-  );
+        Retour
+      </button>
+    </Dialog>
+  )
 }
