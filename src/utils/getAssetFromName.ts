570df48b379a5cf6231012db339322c758f09c0a
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-03-20 15:11:33 +0100

    update random bg assets

--- src/utils/getAssetFromName.ts
+++ src/utils/getAssetFromName.ts
@@ -1,15 +1,4 @@
-type AssetName =
-  | 'mill'
-  | 'borie'
-  | 'venus'
-  | 'deer'
-  | 'elefant'
-  | 'truelle'
-  | 'brouette'
-  | 'king'
-  | 'family'
-
-export const ASSETS: Record<AssetName, string> = {
+export const ASSETS = {
   mill: '3agj2s4Uqz2gIjkxAoVIGy',
   borie: '7wxSIV2ollwz7lEAxlbpt4',
   venus: '4ImM2j4tL1z5Yg7yuiz7SR',
@@ -19,4 +8,7 @@ export const ASSETS: Record<AssetName, string> = {
   brouette: '3TgLWOnNssc61Cc32QO0wK',
   king: '5x2dGJ9NUm1pjaMhQnUaXB',
   family: '5r5TVJ2Uk2ja0vFRoc0y63',
+  duck: '3JBqFnuMTu1pbSFS4ifafO',
+  shark: 'hIjGc51v69UNChLdhQZbV',
+  popeye: '4l7ViTUNNB4XcvbpaLGb3s',
 }
