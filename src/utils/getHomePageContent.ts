1dbd4b71b3a3abca43871bd91abe51c89a19a05e
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-04-16 12:44:45 +0200

    add header utilities

---
+++ src/utils/getHomePageContent.ts
@@ -0,0 +1,12 @@
+import getAssetFromId from '@/queries/getAssetFromId'
+import getHomePage from '@/queries/getHomePage'
+import { ASSETS, TITLES } from './assetsIds'
+
+export default async function getHomePageContent() {
+  const { homePage, error } = await getHomePage()
+  const { image: brouette } = await getAssetFromId(ASSETS.brouette)
+  const { image: tertiary } = await getAssetFromId(TITLES.tertiary, {
+    size: -1,
+  })
+  return { homePage, error, brouette, tertiary }
+}
