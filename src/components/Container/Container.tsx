e8a28bb3b87233d71f2f87c77721afd6c99e4d24
Adrien Gentizon <adrien.gentizon@gmail.com>
2023-04-16 12:46:17 +0200

    suspense on random background

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { BackGroundRandom } from './BackGroundRandom/BackGroundRandom'
 import { ContentfulAsset } from '@/types'
 
@@ -10,22 +11,24 @@ export default function Container({
   children,
 }: React.PropsWithChildren<PropsType>) {
   return (
-    <BackGroundRandom assets={assets}>
-      <div
-        className="container"
-        style={{
-          backgroundColor: 'rgb(255 255 255 / 1)',
-          minHeight: '100vh',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          margin: '0 auto',
-          boxShadow:
-            'rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.1) 0px 8px 10px -6px',
-        }}
-      >
-        {children}
-      </div>
-    </BackGroundRandom>
+    <Suspense>
+      <BackGroundRandom assets={assets}>
+        <div
+          className="container"
+          style={{
+            backgroundColor: 'rgb(255 255 255 / 1)',
+            minHeight: '100vh',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            margin: '0 auto',
+            boxShadow:
+              'rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.1) 0px 8px 10px -6px',
+          }}
+        >
+          {children}
+        </div>
+      </BackGroundRandom>
+    </Suspense>
   )
 }
